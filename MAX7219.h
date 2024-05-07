b95eb6454b3d488f1f78af71575a6a44e216d559
Truong Bang <169121254+trbangspkt@users.noreply.github.com>
2024-05-07 16:39:17 +0700

    Delete MAX7219.h

--- MAX7219.h
+++
@@ -1,6 +0,0 @@
-void sendDataSPI(uint8_t address, uint8_t value);
-void loadSPI(void);
-void initMax7219(void);
-void display3Number(float in1, float in2, float in3);
-void display1Angle(float in);
-void displayTemp(float in);
